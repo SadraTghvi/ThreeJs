9f1bb213ec912349357f7c29d4fca128d1bb3c5b
sadra <sadrataghavi1383@gmail.com>
2023-06-18 17:31:58 +0330

    feat(hero): create a sphere and a floor under it

--- src/Screens/Home/HeroSection.tsx
+++ src/Screens/Home/HeroSection.tsx
@@ -7,10 +7,15 @@ import './style/herosection.scss'
 const HeroSection: FC = () => {
     return (
         <div className='hero-container'>
-            <Canvas>
-                <mesh>
-                    <boxGeometry args={[5, 2, 1]} />
+            <Canvas camera={{ position: [0, 1, 8] }}>
+                <mesh position={[0, 1, 0]}>
+                    <sphereGeometry args={[1, 20, 20]} />
                     <meshStandardMaterial color={'white'} />
+                </mesh>
+
+                <mesh rotation={[-AngleToRadian(90), 0, 0]}>
+                    <planeGeometry args={[5, 5]} />
+                    <meshStandardMaterial color={'blue'} />
                 </mesh>
 
                 {/*  */}
