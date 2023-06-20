faef8873c66cd94b65822cc43e7c65174ea19a73
sadra <sadrataghavi1383@gmail.com>
2023-06-20 15:46:16 +0330

    feat(hero): lights and shadows

--- src/Screens/Home/HeroSection.tsx
+++ src/Screens/Home/HeroSection.tsx
@@ -1,38 +1,62 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
+import { SpotLight } from 'three'
 import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
 
 import './style/herosection.scss'
 
 const HeroSection: FC = () => {
     const controls = useRef<OrbitControlsImpl>(null)
+    const lightControls = useRef<SpotLight>(null)
 
     useFrame(state => {
-        const { x, y } = state.mouse
-        controls.current?.setAzimuthalAngle(-AngleToRadian(x * 250))
-        controls.current?.setPolarAngle(y + 1)
-        controls.current?.update()
+        const { x } = state.mouse
+        // controls.current?.setAzimuthalAngle(-AngleToRadian(x * 250))
+        // controls.current?.setPolarAngle(y + 1)
+        // controls.current?.update()
+
+        lightControls.current?.position.setX(x * 50)
     })
+
+    useEffect(() => {
+        console.log(lightControls.current)
+    }, [lightControls.current])
 
     return (
         <>
-            <PerspectiveCamera makeDefault position={[0, 1, 8]} />
-            <OrbitControls ref={controls} maxPolarAngle={1.5} enablePan />
+            <PerspectiveCamera makeDefault position={[0, 1, 15]} />
+            <OrbitControls
+                ref={controls}
+                maxPolarAngle={1.5}
+                enablePan
+                autoRotate
+            />
 
-            <mesh position={[0, 1, 0]}>
-                <sphereGeometry args={[1, 20, 20]} />
+            {/* ball */}
+            <mesh position={[0, 2, 0]} castShadow>
+                <sphereGeometry args={[1]} />
                 <meshStandardMaterial color={'white'} />
             </mesh>
+            {/* ball-end */}
 
-            <mesh rotation={[-AngleToRadian(90), 0, 0]}>
-                <planeGeometry args={[5, 5]} />
+            {/* floor */}
+            <mesh rotation={[-AngleToRadian(90), 0, 0]} receiveShadow>
+                <planeGeometry args={[20, 20]} />
                 <meshStandardMaterial color={'blue'} />
             </mesh>
+            {/* floor-end */}
 
             {/*  */}
-            <ambientLight args={['white']} intensity={1} />
+            {/* <ambientLight args={['white']} intensity={1} /> */}
+            <spotLight
+                ref={lightControls}
+                args={['white', 1]}
+                position={[0, 5, 0]}
+                distance={50}
+                castShadow
+            />
             {/*  */}
         </>
     )
